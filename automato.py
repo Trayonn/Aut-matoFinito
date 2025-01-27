c1e887a5fa19bdbe2517ebfeea25ee98b74ecf55
Lucas Luiz <lurani2003@gmail.com>
2025-01-27 04:53:45 -0300

    Update automato.py

--- automato.py
+++ automato.py
@@ -1,5 +1,5 @@
-#Autômato Finito CC_D1
-#Lucas Luiz Moreira Urani, Kauan Mendes Caetano, Guilherme Bastos, João Victor Souza
+#Autômato Finito
+#Lucas Luiz Moreira Urani
 
 # Função para encontrar todos os caminhos em um grafo
 def find_all_paths(graph, start, end, path=[]):
